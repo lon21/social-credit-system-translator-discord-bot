6f1888fe87af43dd7cc3783393a516c620d1099f
lon21 <lon21@lon21.pl>
2022-10-10 15:42:28 +0200

    Sr, and cheese language added

---
+++ src/commands/cheese.command.ts
@@ -0,0 +1,18 @@
+import BotCommand from '../types/BotCommand';
+
+export default <BotCommand> {
+	name: 'cheese',
+	desc: 'Przetłumacz cokolwiek na ser',
+	aliases: ['ser'],
+	run: async ({ msg, args }) => {
+		if (!args) return msg.reply('Proszę pana, nie mogę przetłumaczyć czegoś czego nie ma!');
+		const message = await msg.reply('Proszę mi dać sekundę, już tłumaczę!');
+		const trans = [];
+		const chars = ['ser', 'cheese', '🧀', 'CHEESE', 'SER', 'CHEese'];
+		args.forEach(() => {
+			const char = chars[Math.floor(Math.random() * chars.length)];
+			trans.push(char);
+		});
+		message.edit(`Pana tłumaczenie to:\n${trans.join(' ')}`);
+	},
+}
